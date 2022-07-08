b7008de35f5ee84fa6b6a45c852656f337863fdb
#R26 <93254944+ikiwz-XD@users.noreply.github.com>
2022-07-08 16:41:43 +0700

    Update Fbh1.py

--- Fbh1.py
+++ Fbh1.py
@@ -417,7 +417,7 @@ def lah():
 def grup():
 	print('')
 	id = input(f'{x}>> Masukkan Username Atau Idz Grup : ')
-	ua = 'Mozilla/5.0 (SymbianOS/9.3; Series60/3.2 NokiaE52-1/052.003; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/525 (KHTML, like Gecko) Version/3.0 BrowserNG/7.2.6.2 3gpp-gba'
+	ua = 'NokiaX3-00/2.0 (p) Profile/MIDP-2.1 Configuration/CLDC-1.1 Mozilla/5.0 AppleWebKit/420+ (KHTML, like Gecko) Safari/420+'
 	miskinlu = {"user-agent": ua}
 	url = "https://mbasic.facebook.com/groups/"+id
 	ses = requests.Session()

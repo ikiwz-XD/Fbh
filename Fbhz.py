c98083f1870f68947fc389091b6a17cb29395f71
ikiwz-XD <93254944+ikiwz-XD@users.noreply.github.com>
2022-07-14 18:24:02 +0700

    Update Fbhz.py

--- Fbhz.py
+++ Fbhz.py
@@ -206,11 +206,11 @@ def menu(my_name,my_id):
 	ip = requests.get("https://api.ipify.org").text
 	gh = 'github.com/ikiwz-XD'
 	cetak(nel('\tSelamat Datang [blue]%s[yellow] ToloL'%(my_name)))
-	print(f' →{p}[•] Your Idz : '+str(my_id))
-	print(f' →{m}[•] Your Ip  : {ip}')
-	print(f' →{k}[•] Github   : {gh}')
+	print(f' →{p}•{m}•{h}•k•}[•] Your Idz : '+str(my_id))
+	print(f' →{m}•{h}•{p}•{[•] Your Ip  : {ip}')
+	print(f' →{k}{•{h}•{p}[•] Github   : {gh}')
 	print('')
-	print{h}('  →[•] 1. Crack Publik  [ fitur √ ]')
+	print('  →[•] 1. Crack Publik  [ fitur √ ]')
 	print('  →[•] 2. Crack Pengikut   [ fitur √ ] ')
 	print('  →[•] 3. Crack Grup       [ Fitur X ] ')
 	print('  →[•] 4. Crack File       [ Fitur √ ]	')

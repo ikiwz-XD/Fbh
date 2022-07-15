a8c1f3f1647223920c0e9b3afb311a33e7eba4a3
ikiwz-XD <93254944+ikiwz-XD@users.noreply.github.com>
2022-07-15 10:29:50 +0700

    Update Fbhz.py

--- Fbhz.py
+++ Fbhz.py
@@ -579,7 +579,7 @@ def setting():
 	print('[•] 2. Akun New ')
 	print('[•] 3. Random  [Rekomended]')
 
-	hu = input('———————————>[•] Pilih  : ')
+	hu = input('———————————> Pilih : ')
 	if hu in ['1','01']:
 		for tua in sorted(id):
 			id2.append(tua)
@@ -849,7 +849,7 @@ def cracktouch(idf,pwv):
 	ua = random.choice(ugen)
 	ua2 = random.choice(ugen2)
 	ses = requests.Session()
-	sys.stdout.write('\r%s [•] %s/%s [!] OK:[+] ☬ CP:%s [-] %s%s%s [=]'%(bi,loop,len(id2),ok,cp,int(pers),str(fff),x));sys.stdout.flush()
+	sys.stdout.write('\r%s [•] %s/%s [!] OK:%s [+] CP:%s [-] %s%s%s [=]'%(bi,loop,len(id2),ok,cp,int(pers),str(fff),x));sys.stdout.flush()
 	for pw in pwv:
 		try:
 			ses.headers.update({'Host': 'touch.facebook.com','cache-control': 'max-age=0','sec-ch-ua-mobile': '?1','upgrade-insecure-requests': '1','user-agent': ua,'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','sec-fetch-site': 'same-origin','sec-fetch-mode': 'cors','sec-fetch-dest': 'empty','accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7'})

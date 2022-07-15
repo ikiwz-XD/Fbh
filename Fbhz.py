24152ae9ba89a27de917269c082e27e37e5184dd
ikiwz-XD <93254944+ikiwz-XD@users.noreply.github.com>
2022-07-15 14:25:30 +0700

    Update Fbhz.py

--- Fbhz.py
+++ Fbhz.py
@@ -575,7 +575,7 @@ def crack_file():
 		setting()
 #-------------[ PENGATURAN-IDZ ]---------------#
 def setting():
-cetak(nel('\n\tPilih Salah Satu : Pake Angka'))
+
 	print(f'{x}{p}[•] 1. Akun Old ')
 	print(f'{b}[•] 2. Akun New ')
 	print(f'{hh}[•] 3. Random {x} {m}[Rekomended]{x}')
